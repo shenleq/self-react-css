dc17c395ed031266a8aa0bc3e001405d39c53f63
shenleq <2313156144@qq.com>
2022-12-28 11:20:07 +0800

    更新

---
+++ src/components/Video/aaa.vue
@@ -0,0 +1,20 @@
+<template>
+  <div slot="A">
+    <div class="aaa">FUCKBB</div>
+  </div>
+</template>
+<script>
+
+  
+  export default {
+    
+  }
+</script>>
+
+<style scoped>
+  .aaa{
+    widows: 20px;
+    height: 20px;
+    background: #762aaa;
+  }
+</style>
